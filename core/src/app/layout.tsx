cb43cad218587df611fc69ec968b755483c8857f
arnaudpfu <arnaudpfundstein.pro@gmail.com>
2025-11-28 11:07:21 +0100

    first commit

---
+++ core/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "./globals.css";
+
+const geistMono = Geist_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+});
+
+const geistSans = Geist({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ example/envs.ts
@@ -0,0 +1,31 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+// export const envs = () =>
+//     createEnv({
+
+export const envs = createEnv({
+        client: {
+            NEXT_PUBLIC_CAPTCHA_SITE_KEY: z.string().optional(),
+            NEXT_PUBLIC_CREATOREM_URL: z.string().min(1),
+            NEXT_PUBLIC_MARKETING_URL: z.string().min(1),
+            NEXT_PUBLIC_GITHUB_REPO_LINK: z.string().optional(),
+            NEXT_PUBLIC_STRIPE_PRODUCT_REPO_ACCESS: z.string().min(1),
+        },
+        server: {
+            NOTION_POSTS_DB_ID: z.string().min(1),
+            NOTION_ROADMAP_DB_ID: z.string().min(1),
+            NOTION_INTEGRATION_SECRET: z.string().min(1),
+            GITHUB_TOKEN: z.string().min(1),
+            GITHUB_REPO_OWNER: z.string().min(1),
+            GITHUB_REPO_NAME: z.string().min(1),
+            AUTH_WEBHOOK_SECRET: z.string().min(1),
+            GITHUB_PROJECT_OWNER: z.string().min(1),
+            GITHUB_PROJECT_REPOSITORY: z.string().min(1),
+            GITHUB_APP_ID: z.string().min(1),
+            GITHUB_APP_PRIVATE_KEY: z.string().min(1),
+            GITHUB_MODIFIED_TIME_TOKEN: z.string().min(1),
+        },
+        runtimeEnv: process.env,
+        emptyStringAsUndefined: true,
+    });
